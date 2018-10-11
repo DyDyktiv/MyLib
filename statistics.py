76040beff8656159741fd9985a39a9c8f90de972
Pavel <Drevniu@ya.ru>
2018-10-11 22:47:21 +0300

    Ver 2.0. not working

--- statistics.py
+++ statistics.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import re
+import time
 
 
 def scanning(path=os.getcwd(), homepath='', depth=-1):
@@ -46,10 +47,38 @@ def scanning(path=os.getcwd(), homepath='', depth=-1):
 class Pyfile:
     def __init__(self, name):
         self.name = name
+        self.checks = []
+        self.scan()
+
+    def scan(self):
+        if os.path.exists(self.name):
+            size = os.path.getsize(self.name)
+            with open(self.name, encoding='utf-8') as file:
+                classes = 0
+                defines = 0
+                lines = 0
+                for line in file:
+                    if re.match(r'[\t]*class ', line):
+                        classes += 1
+                    if re.match(r'[\t]*def ', line):
+                        defines += 1
+                    lines += 1
+                self.checks.append(CheckPoint(time.time(), classes, defines, lines, size))
+        else:
+            self.checks.append(CheckPoint(time.time()))
+
+
+class CheckPoint:
+    def __init__(self, date, classes=0, defines=0, lines=0, sizes=0):
+        self.date = date
+        self.classes = classes
+        self.defines = defines
+        self.lines = lines
+        self.bytes = sizes
 
 
 if __name__ == '__main__':
     pyfiles = scanning()
     for fp in pyfiles:
-        print(fp)
+        Pyfile(fp)
     input()
